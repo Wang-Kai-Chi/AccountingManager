2348245f07757530793d55ea18fde75caa18f42e
catch wang <ericwangcatch@gmail.com>
2022-11-10 14:41:33 +0800

    add MyApplicationBackEnd

--- src/main/java/com/ericwang/jaccount/Main.java
+++ src/main/java/com/ericwang/jaccount/Main.java
@@ -1,6 +1,5 @@
 package com.ericwang.jaccount;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
@@ -8,49 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to JAccountingManager!!");
         try {
-            MyApplication myApplication = new MyApplication(new MySQLConnectionBuilder("test123"));
+            MyApplicationBackEnd myApplicationBackEnd = new MyApplicationBackEnd(new MySQLConnectionBuilder("test123"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-}
-
-class MyApplication {
-    public MyApplication(MySQLConnectionBuilder mySQLConnectionBuilder) {
-        System.out.println("connection start");
-
-        ApplicationActor applicationActor = new ApplicationActor();
-        applicationActor.start(mySQLConnectionBuilder.getConnection());
-
-        MemberRepository memberRepository = new MemberRepository();
-        memberRepository.refresh(applicationActor.getController());
-
-        for (Member m : memberRepository.getMemberList())
-            System.out.println(m);
-    }
-}
-
-class ApplicationActor {
-    private DBController controller;
-
-    public ApplicationActor() {
-        controller = new DBController();
-    }
-
-    public void start(Connection connection) {
-        String sql = "SELECT * FROM member";
-        try {
-            controller.query(sql, connection);
-        //controller.add();
-
-//            controller.update(controller.getRows(), controller.getHeaders()[1], "John");
-//            controller.update(controller.getRows(), controller.getHeaders()[2], "1989-10-29");
-//            controller.update(controller.getRows(), controller.getHeaders()[3], "solder");
-        } catch (SQLException ignored) {}
-        System.out.println("Query command: " + sql);
-    }
-
-    public DBController getController() {
-        return controller;
     }
 }
---
+++ src/main/java/com/ericwang/jaccount/MyApplicationBackEnd.java
@@ -0,0 +1,45 @@
+package com.ericwang.jaccount;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+class MyApplicationBackEnd {
+    public MyApplicationBackEnd(MySQLConnectionBuilder mySQLConnectionBuilder) {
+        System.out.println("connection start");
+
+        ApplicationActor applicationActor = new ApplicationActor();
+        applicationActor.start(mySQLConnectionBuilder.getConnection());
+
+        MemberRepository memberRepository = new MemberRepository();
+        memberRepository.refresh(applicationActor.getController());
+
+        for (Member m : memberRepository.getMemberList())
+            System.out.println(m);
+    }
+
+    private class ApplicationActor {
+        private DBController controller;
+
+        public ApplicationActor() {
+            controller = new DBController();
+        }
+
+        public void start(Connection connection) {
+            String sql = "SELECT * FROM member";
+            try {
+                controller.query(sql, connection);
+                //controller.add();
+
+//            controller.update(controller.getRows(), controller.getHeaders()[1], "John");
+//            controller.update(controller.getRows(), controller.getHeaders()[2], "1989-10-29");
+//            controller.update(controller.getRows(), controller.getHeaders()[3], "solder");
+            } catch (SQLException ignored) {
+            }
+            System.out.println("Query command: " + sql);
+        }
+
+        public DBController getController() {
+            return controller;
+        }
+    }
+}
