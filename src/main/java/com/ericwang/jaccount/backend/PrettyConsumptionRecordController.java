f4bf133ca0af4a8b30328fdebb6e09640b08fb6c
catch wang <ericwangcatch@gmail.com>
2022-11-14 21:36:53 +0800

    add insertNewRecordIfIdIsZero method to PrettyConsumptionRecordController

--- src/main/java/com/ericwang/jaccount/backend/PrettyConsumptionRecordController.java
+++ src/main/java/com/ericwang/jaccount/backend/PrettyConsumptionRecordController.java
@@ -1,13 +1,16 @@
 package com.ericwang.jaccount.backend;
 
+import com.ericwang.jaccount.backend.scr.SingleConsumptionRecord;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PrettyConsumptionRecordController {
     private ArrayList<PrettyConsumptionRecord> recordList;
-	private PrettyConsumptionRecordRepository repo;
-	private String[] headers;
+    private PrettyConsumptionRecordRepository repo;
+    private String[] headers;
 
-    public PrettyConsumptionRecordController(){
+    public PrettyConsumptionRecordController() {
         recordList = new ArrayList<>();
         headers = new String[2];
     }
@@ -18,31 +21,62 @@ public class PrettyConsumptionRecordController {
     }
 
     public void refresh() {
-		if (repo!=null) {
-			recordList.clear();
+        if (repo != null) {
+            recordList.clear();
 
-			for (int i = 1; i < repo.getRows() + 1; i++) {
-				recordList.add(new PrettyConsumptionRecord(
+            for (int i = 1; i < repo.getRows() + 1; i++) {
+                recordList.add(new PrettyConsumptionRecord(
                         Integer.parseInt(repo.getData(i, 1)),
-						Integer.parseInt(repo.getData(i, 2)),
-						repo.getData(i, 3),
+                        Integer.parseInt(repo.getData(i, 2)),
+                        repo.getData(i, 3),
                         repo.getData(i, 4),
-						repo.getData(i, 5)));
-			}
-			headers = repo.getHeaders();
-		}
-	}
+                        repo.getData(i, 5)));
+            }
+            headers = repo.getHeaders();
+        }
+    }
+
+    public void insertNewRecordIfIdIsZero(Object[] categories) {
+        if (repo != null) {
+            for (PrettyConsumptionRecord p : recordList) {
+                if (p.getId() == 0) {
+                    try {
+                        SingleConsumptionRecord s = getConsumption(p, categories);
+                        repo.add(s);
+
+                        System.out.println(s);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    public SingleConsumptionRecord getConsumption(PrettyConsumptionRecord p, Object[] categories) {
+        SingleConsumptionRecord s = new SingleConsumptionRecord(p.getId());
+        s.setAmount_of_money(p.getAmountOfMoney());
+        s.setDate(p.getDate());
+        s.setCategory(p.getCategory());
+
+        for (int i = 0; i < categories.length; i++) {
+            if (s.getCategory().equals(categories[i]))
+                s.setCategory_id(i);
+        }
+        return s;
+    }
 
     public void print() {
-		for (PrettyConsumptionRecord c : recordList)
-			System.out.println(c);
-	}
+        for (PrettyConsumptionRecord c : recordList)
+            System.out.println(c);
+    }
 
-	public ArrayList<PrettyConsumptionRecord> getRecordList() {
-		return recordList;
-	}
+    public ArrayList<PrettyConsumptionRecord> getRecordList() {
+        return recordList;
+    }
 
-	public String[] getHeaders() {
-		return headers;
-	}
+    public String[] getHeaders() {
+        return headers;
+    }
 }
