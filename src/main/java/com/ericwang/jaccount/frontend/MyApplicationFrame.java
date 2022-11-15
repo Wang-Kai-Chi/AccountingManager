89dfaae9267f84169bc0b5fc29bdd8bda678ef52
catch wang <ericwangcatch@gmail.com>
2022-11-15 11:58:17 +0800

    update MyApplicationFrame(ui and ux update)

--- src/main/java/com/ericwang/jaccount/frontend/MyApplicationFrame.java
+++ src/main/java/com/ericwang/jaccount/frontend/MyApplicationFrame.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -12,93 +13,105 @@ import com.ericwang.jaccount.backend.PrettyConsumptionRecord;
 import com.ericwang.jaccount.backend.PrettyConsumptionRecordController;
 
 public class MyApplicationFrame extends JFrame {
-    private CashFlowTable cashFlowTable;
-    private InsertDataDialog insertDialog;
-    private UpdateDataDialog updateDialog;
-    private PrettyConsumptionRecordController controller;
-    private Object[] categories;
+	private CashFlowTable cashFlowTable;
+	private InsertDataDialog insertDialog;
+	private UpdateDataDialog updateDialog;
+	private PrettyConsumptionRecordController controller;
+	private Object[] categories;
 
-    public MyApplicationFrame(PrettyConsumptionRecordController controller, Object[] categories) {
-        super("記帳本");
-        this.controller = controller;
-        this.categories = categories;
+	public MyApplicationFrame(PrettyConsumptionRecordController controller, Object[] categories) {
+		super("記帳本");
+		this.controller = controller;
+		this.categories = categories;
 
-        setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
 
-        cashFlowTable = new CashFlowTable();
-        cashFlowTable.initTable(controller);
-        JScrollPane jsp = new JScrollPane(cashFlowTable);
-        add(jsp, BorderLayout.CENTER);
+		cashFlowTable = new CashFlowTable();
+		cashFlowTable.initTable(controller);
+		JScrollPane jsp = new JScrollPane(cashFlowTable);
+		add(jsp, BorderLayout.CENTER);
 
-        ManagePanel managePanel = new ManagePanel();
-        add(managePanel, BorderLayout.NORTH);
+		ManagePanel managePanel = new ManagePanel();
+		add(managePanel, BorderLayout.SOUTH);
 
-        initDialog();
+		initDialog();
 
-        setSize(800, 480);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
+		setSize(800, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
-    private void initDialog() {
-        insertDialog = new InsertDataDialog(this, categories);
-        insertDialog.setController(controller);
-        insertDialog.setTable(cashFlowTable);
-        
-        updateDialog = new UpdateDataDialog(this, categories);
-        updateDialog.setController(controller);
-        updateDialog.setTable(cashFlowTable);
-    }
+	private void initDialog() {
+		insertDialog = new InsertDataDialog(this, categories);
+		insertDialog.setController(controller);
+		insertDialog.setTable(cashFlowTable);
 
-    private class ManagePanel extends JPanel {
-        private JButton addB, updateB, deleteB, refreshB;
+		updateDialog = new UpdateDataDialog(this, categories);
+		updateDialog.setController(controller);
+		updateDialog.setTable(cashFlowTable);
+	}
 
-        public ManagePanel() {
-            super(new FlowLayout());
+	private class ManagePanel extends JPanel {
+		private JButton addB, updateB, deleteB, refreshB;
 
-            addB = new JButton("新增");
-            updateB = new JButton("更新");
-            refreshB = new JButton("刷新");
-            deleteB = new JButton("刪除");
+		public ManagePanel() {
+			super(new FlowLayout());
 
-            add(addB);
-            add(updateB);
-            add(deleteB);
-            add(refreshB);
+			addB = new JButton("新增");
+			updateB = new JButton("更新");
+			refreshB = new JButton("刷新");
+			deleteB = new JButton("刪除");
 
-            setActionListeners();
-        }
+			add(addB);
+			add(updateB);
+			add(deleteB);
+			add(refreshB);
 
-        private void setActionListeners() {
-            addB.addActionListener(e -> {
-                insertDialog.setVisible(true);
-                cashFlowTable.add();
-            });
-            
-            deleteB.addActionListener(e->{
-            	controller.deleteFromDb(getRecordFromSelectedRow());
-            	cashFlowTable.initTable(controller);
-            });
-            
-            updateB.addActionListener(e->{
-            	updateDialog.setRecord(getRecordFromSelectedRow());
-            	updateDialog.setVisible(true);
-            });
+			setActionListeners();
+		}
 
-            refreshB.addActionListener(e -> {
-                controller.refresh();
-                cashFlowTable.initTable(controller);
-            });
-        }
-        
-        private PrettyConsumptionRecord getRecordFromSelectedRow() {
-        	int id = (int) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 0);
-        	int money = (int) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 1);
-        	String date = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 2);
-        	String category = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 3);
-        	String des = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 4);
-        	
-        	return new PrettyConsumptionRecord(id,money,date,category,des);
-        }
-    }
+		private void setActionListeners() {
+			addB.addActionListener(e -> {
+				insertDialog.setVisible(true);
+				cashFlowTable.add();
+			});
+
+			deleteB.addActionListener(e -> {
+				try {
+					int check = JOptionPane.showConfirmDialog(this, "確定要刪除這筆資料嗎", "刪除", JOptionPane.DEFAULT_OPTION);
+					
+					if (check == JOptionPane.YES_OPTION) {
+						controller.deleteFromDb(getRecordFromSelectedRow());
+						cashFlowTable.initTable(controller);
+					}
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog(this, "請先選擇資料");
+				}
+			});
+
+			updateB.addActionListener(e -> {
+				try {
+					updateDialog.setRecord(getRecordFromSelectedRow());
+					updateDialog.setVisible(true);
+				} catch (Exception e1) {
+					JOptionPane.showMessageDialog(this, "請先選擇資料");
+				}
+			});
+
+			refreshB.addActionListener(e -> {
+				controller.refresh();
+				cashFlowTable.initTable(controller);
+			});
+		}
+
+		private PrettyConsumptionRecord getRecordFromSelectedRow() {
+			int id = (int) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 0);
+			int money = (int) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 1);
+			String date = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 2);
+			String category = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 3);
+			String des = (String) cashFlowTable.getValueAt(cashFlowTable.getSelectedRow(), 4);
+
+			return new PrettyConsumptionRecord(id, money, date, category, des);
+		}
+	}
 }
