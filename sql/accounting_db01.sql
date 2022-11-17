311ee2da07a70a6b29262e114917a82303695aac
catch wang <ericwangcatch@gmail.com>
2022-11-17 15:00:20 +0800

    add diagrams and update sql

--- sql/accounting_db01.sql
+++ sql/accounting_db01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 11, 2022 at 08:25 AM
+-- Generation Time: Nov 17, 2022 at 06:51 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -63,16 +63,26 @@ CREATE TABLE `single_consumption_record` (
 --
 
 INSERT INTO `single_consumption_record` (`id`, `amount_of_money`, `date`, `category_id`, `description`) VALUES
-(1, 100, '2022-11-01', 1, NULL),
-(2, 150, '2022-11-01', 1, NULL),
-(3, 80, '2022-11-02', 2, NULL),
-(4, 120, '2022-11-02', 2, NULL),
-(5, 180, '2022-11-02', 3, NULL),
+(1, 54305, '2022-10-26', 6, '更新測試'),
+(4, 120, '2022-11-12', 2, 'fkckirhcoerh'),
 (6, 180, '2022-11-03', 1, NULL),
-(7, 100, '2022-11-04', 1, NULL),
+(7, 9876, '2022-10-04', 5, 'test update'),
 (8, 100, '2022-11-04', 1, NULL),
-(9, 100, '2022-11-05', 1, 'dinner'),
-(10, 100, '2022-11-05', 1, '');
+(10, 100, '2022-11-05', 1, ''),
+(11, 150, '2022-11-10', 4, 'test'),
+(12, 200, '2022-11-08', 6, 'test2'),
+(14, 90, '2022-11-02', 3, 'bus'),
+(15, 543, '2022-11-22', 2, 'test update'),
+(17, 1000, '2022-10-10', 5, 'test3'),
+(18, 1300, '2022-10-17', 5, '中文'),
+(20, 900, '2022-10-27', 3, 'update33445'),
+(22, 9999, '2022-10-28', 6, 'aaaaaaaaaaaaaaaaaa'),
+(24, 4000, '2022-11-14', 4, '測試新增'),
+(25, 299, '2022-11-15', 1, '測試搜尋今日'),
+(26, 9981, '2021-10-12', 3, 'test search year'),
+(27, 43423, '2021-09-08', 4, 'testtt'),
+(28, 300, '2021-10-21', 2, 'test23456'),
+(29, 1000, '2022-11-16', 2, '搜尋時新增');
 
 --
 -- Indexes for dumped tables
@@ -104,7 +114,7 @@ ALTER TABLE `consumption_category`
 -- AUTO_INCREMENT for table `single_consumption_record`
 --
 ALTER TABLE `single_consumption_record`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
